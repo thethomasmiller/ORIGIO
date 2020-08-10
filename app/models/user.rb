20c09b484108930590ffff0545609145e6076c6f
Thomas Miller <thomasmiller@Thomass-Air.home>
2020-08-10 14:19:13 -0400

    Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :ideas, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
+
 end
