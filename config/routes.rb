db14137dbc453bfeb2a383e4aae83fb426cb154e
Thomas Miller <thomasmiller@Thomass-Air.home>
2020-08-10 14:27:48 -0400

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :users
+
+  resources :comments, only: :index
+  resources :ideas
+  resources :users, only: :create
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  put '/ideas/:idea_id/comments/:id', to: 'ideas#add_comment'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
