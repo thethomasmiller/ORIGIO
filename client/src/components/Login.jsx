9441711622af03ee13ad178ac6fb9c1d8e0282b5
Thomas Miller <thomasmiller@Thomass-Air.home>
2020-08-12 10:43:29 -0400

    Handle Change

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Header from './header/Header'
 import Footer from './footer/Footer'
 
 export default class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  
   render() {
     return (
       <div>
--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {Route} from 'react-router-dom'
-import Login from '..Login'
+import Login from '../Login'
 import Register from '../Register'
 
 export default class Main extends Component {
