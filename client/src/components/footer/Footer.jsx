ba204331c7de8950863a6b545e7ff5aeb4d053a3
Thomas Miller <thomasmiller@Thomass-Air.home>
2020-08-18 15:17:34 -0400

    Login CSS

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,12 +4,12 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className="footer">
-    <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
-    <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
-    <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
-    <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
-    <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
-    <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
-  </div>
+      <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
+      <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
+      <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
+      <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
+      <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
+      <div className='about'><div>a</div><div>b</div><div>o</div><div>u</div><div>t</div></div>
+    </div>
   )
 }
